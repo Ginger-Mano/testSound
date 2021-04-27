4df6c078cb7f45b380c23f1f67ac83252caddb85
Manon Sainton <manonsainton.dev@gmail.com>
2021-04-27 12:19:38 -0400

    btn css

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ let sound1 = new Audio('https://freesound.org/data/previews/316/316857_4486188-l
 let sound2 = new Audio('https://freesound.org/data/previews/196/196877_2526129-lq.mp3')
 
 let useSound = () => {
+    let soundDiv = document.createElement("div")
+        soundDiv.className = "soundDiv"
+
+        
+
     let playBtn = document.createElement('button')
-    playBtn.className = "playBtn"
-    playBtn.innerText = "Play"
+        playBtn.className = "playBtn"
+        playBtn.innerText = "Play"
 
     playBtn.addEventListener("click", (evt) => {
         sound1.play()
     })
 
+
     let stopBtn = document.createElement('button')
-    stopBtn.className = "stopBtn"
-    stopBtn.innerText = "Stop"
+        stopBtn.className = "stopBtn"
+        stopBtn.innerText = "Stop"
 
     stopBtn.addEventListener("click", (evt) => {
         sound2.play()
     })
 
-    appBody.append(playBtn, stopBtn)
+
+    soundDiv.append(playBtn, stopBtn)
+    appBody.append(soundDiv)
 }
 
 useSound()
