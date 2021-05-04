f609869c2b27f755ad13f8b9c5b47a1299c604c1
Manon Sainton <manonsainton.dev@gmail.com>
2021-05-04 10:43:41 -0400

    center buttons

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let appBody = document.querySelector('.main')
 let sound1 = new Audio('https://freesound.org/data/previews/316/316857_4486188-lq.mp3');
-let sound2 = new Audio('https://freesound.org/data/previews/196/196877_2526129-lq.mp3')
+let sound2 = new Audio('https://freesound.org/data/previews/196/196877_2526129-lq.mp3');
+
 
 let useSound = () => {
     let soundDiv = document.createElement("div")
         soundDiv.className = "soundDiv"
 
-        
+    let playBtnDiv =  document.createElement('div')
+        playBtnDiv.className = "playBtnDiv"
+
+    let playTitle = document.createElement('h2')
+        playTitle.innerHTML = "Play"
 
     let playBtn = document.createElement('button')
         playBtn.className = "playBtn"
@@ -16,6 +21,11 @@ let useSound = () => {
         sound1.play()
     })
 
+    let stopTitle = document.createElement('h2')
+        stopTitle.innerHTML = "Stop"
+
+    let stopBtnDiv =  document.createElement('div')
+        stopBtnDiv.className = "stopBtnDiv"
 
     let stopBtn = document.createElement('button')
         stopBtn.className = "stopBtn"
@@ -25,8 +35,9 @@ let useSound = () => {
         sound2.play()
     })
 
-
-    soundDiv.append(playBtn, stopBtn)
+    playBtnDiv.append(playTitle, playBtn)
+    stopBtnDiv.append(stopTitle, stopBtn)
+    soundDiv.append(playBtnDiv, stopBtnDiv)
     appBody.append(soundDiv)
 }
 
